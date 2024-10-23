d81a48e30659daa8bb6007a435c0c231d0c97c42
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-10-23 14:07:41 +0330

    feat(production): pushed to liara

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 }
